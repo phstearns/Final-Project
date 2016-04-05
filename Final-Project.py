64c660ea8dfaff928935c5f6deafb752ca278f79
phstearns <phstearns13@gmail.com>
2016-04-05 14:40:03 -0400

    Updated from Brython Server: 4/5/2016 2:40:03 PM

--- Final-Project.py
+++ Final-Project.py
@@ -11,20 +11,12 @@ import math
 from time import time
 
 class AlienShooterGame(App):
-    def space(self, evt):
-        if self.state in ['instructions', 'gameover']:
-            for t in self.tsprites.values():
-                t.visible = False
-            self.state = 'playing'
-            self.Tlast = time()
-            evt.consumed = True
-            self.ship1.newgame()
-            self.ship2.newgame()
+    def space(self, event):
 
     def __init__(self, width, height):
-        global sun
+        
         super().__init__(width, height)
-        bg_asset = ImageAsset("images/starfield.jpg")
+        bg_asset = ImageAsset("images/sky-lights-space-dark.jpg")
         txt_asset = TextAsset("Control Dwon 3: Tokyo Drift Mode", width = 300, align ='center', style='40px Times', fill=Color(0xff2222,1)) 
         bg = Sprite(bg_asset, (0,0))
         bg = Sprite(bg_asset, (0,512))
