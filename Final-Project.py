c7b8e235013e513fcea2819b8cbb0e3da5c9bdfe
phstearns <phstearns13@gmail.com>
2016-04-18 18:59:43 -0400

    Updated from Brython Server: 4/18/2016 6:59:43 PM

--- Final-Project.py
+++ Final-Project.py
@@ -19,7 +19,7 @@ while quit == False:
         bin=bin(int(binascii.hexlify(text), 16))
         print(bin)
     if i=="b":
-        n = int("input: ")
+        n = int(input("binary: ", 2))
         bin2=binascii.unhexlify('%x' % n)
         print(bin2)
     if i=="q":
