5d43ceb70226f483d814b2f517f39de1cbc30244
phstearns <phstearns13@gmail.com>
2016-04-18 14:53:12 -0400

    Updated from Brython Server: 4/18/2016 2:53:12 PM

--- Final-Project.py
+++ Final-Project.py
@@ -16,26 +16,8 @@ while quit == False:
         i = input("Enter t to translate text to binary, b to translate binary to text, or q to quit: ")
     if i=="t":
         text=input("text: ")
-        bin(int(binascii.hexlify(text), 16))
-        let=[]
-        kelt=[]
-        comb =[]
-        if m>k:
-            count = key * int((m-(m%k))/k)
-            trun = key[0:(m%k)]
-            newkey = count + trun
-        elif k>m:
-            newkey = key[0:m] 
-            print(newkey, message)
-        for x in range (0,m):
-            let.append(associations.find(message[x]))
-        for y in range (0,m):
-            kelt.append(associations.find(newkey[y]))
-        for c in range (0,len(kelt)):
-            r = (let[c] + kelt[c]) % 85
-            comb.append(associations[r])
-        print("".join([x for x in comb]), end="")
-        print()
+        bin=bin(int(binascii.hexlify(text), 16))
+        print(bin)
     if i=="b":
         kelt=[]
         let=[]
