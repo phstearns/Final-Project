5e9a48f1272f0b2ecd895c586971c60dba445a6c
phstearns <phstearns13@gmail.com>
2016-05-27 14:44:29 -0400

    Updated from Brython Server: 5/27/2016 2:44:31 PM

--- Final-Project.py
+++ Final-Project.py
@@ -8,19 +8,6 @@ Write and submit a program that is your Final Project.
 import binascii
 quit = False
 
-def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-    bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
-
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
-    n = int(bits, 2)
-    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
-
-def int2bytes(i):
-    hex_string = '%x' % i
-    n = len(hex_string)
-    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
-
 while quit == False:
     i = input("Enter a to translate ascii to binary, u to translate Unicode to binary, ba to tranlate binary to ascii, bu to translate binary to Unicode, or q to quit: ")
     if i not in('a', 'u', 'ba', 'bu', 'q'):
