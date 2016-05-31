4333331f3f21a0a03a9d116fd1f52f6601f7879b
phstearns <phstearns13@gmail.com>
2016-05-31 14:13:52 -0400

    Updated from Brython Server: 5/31/2016 2:13:53 PM

--- Final-Project.py
+++ Final-Project.py
@@ -5,7 +5,6 @@ Credit: Python Documentation, Morgan, Mr. Dennison, Stack Overflow
 Assignment: Final-Project
 Write and submit a program that is your Final Project.
 """
-print("Hello, world!")
 import binascii
 quit = False
 
